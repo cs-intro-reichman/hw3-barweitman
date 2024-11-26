4ea39a62ee336640f34fb2a3b605023cd3fd1d1f
Bar Weitman <barweitman@gmail.com>
2024-11-27 00:56:12 +0200

    test

--- Algebra.java
+++ Algebra.java
@@ -6,25 +6,37 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-		System.out.println(minus(7,2));  // 7 - 2
-		System.out.println(minus(2,7));  // 2 - 7
-		System.out.println(times(3,4));  // 3 * 4
-		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-		System.out.println(pow(5,3));      // 5^3
-		System.out.println(pow(3,5));      // 3^5
-		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123)); 
+	    //System.out.println(plus(-8,-5));   // 2 + 3
+		//System.out.println(minus(7,2));  // 7 - 2
+		//System.out.println(minus(2,7));  // 2 - 7
+		//System.out.println(times(-3,-4));  // 3 * 4
+		//System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+		//System.out.println(pow(5,-3));      // 5^3
+		//System.out.println(pow(-3,5));      // 3^5
+		System.out.println(div(1,3));   // 12 / 3    
+   		System.out.println(div(5,-5));    // 5 / 5  
+   		//System.out.println(div(25,7));   // 25 / 7
+		//System.out.println(mod(25,7));   // 25 % 7
+   		//System.out.println(mod(120,6));  // 120 % 6    
+   		//System.out.println(sqrt(36));
+		//System.out.println(sqrt(263169));
+   		//System.out.println(sqrt(76123)); 
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
+		if (x2 < 0 && x1 < 0){
+			while (x2 != 0) {
+				x1--;
+				x2 ++;
+			}
+		}
+		else if (x2 < 0 && x1 > 0) {
+			while (x2 != 0) {
+				x1--;
+				x2++;
+			}
+		}
 		for (int i=0; i<x2; i++){
 			x1++;
 		}
@@ -34,6 +46,18 @@ public class Algebra {
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
+		if (x2 < 0 && x1 < 0){
+			while (x2 != 0) {
+				x1++;
+				x2 --;
+			}
+		}
+		else if (x2 < 0 && x1 > 0) {
+			while (x2 != 0) {
+				x1++;
+				x2--;
+			}
+		}
 		for (int i=0; i<x2; i++){
 			x1--;
 		}		
@@ -42,29 +66,72 @@ public class Algebra {
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
+		if (x1 == 0 || x2 == 0)
+			return 0;
+		else{
 		int x = x1;
+		if (x2 < 0 && x1 < 0){
+			while (x2 != -1) {
+				x1 = plus(x1, x);
+				x2++;
+			}
+		}
+		else if (x2 < 0 && x1 > 0) {
+			while (x2 != -1) {
+				x1 = plus(x1, x);
+				x2++;
+			}
+		}
 		for (int i=1; i<x2; i++){
 			x1 = plus(x1, x);
 		}		
 		return x1;
 	}
-
-	// Returns x^n (for n >= 0)
-	public static int pow(int x, int n) {
-		int y = times(x, x);
-		for (int i=2; i<n; i++){
-			y  = times(y, x);
-		}		
-		return y;
 	}
 
-	// Returns the integer part of x1 / x2 
-	public static int div(int x1, int x2) {
-		int z = 0;
-		while (x1 >= x2) {
+	// Returns x^n (for n >= 0) // 5 -3 = 1/125
+	public static double pow(int x, int n) {
+		if (n == 0)
+			return 1;
+		if (x == 0)
+			return 0;
+		int y = times(x, x); //25
+		if (n > 0)
+		{
+		for (int i=2; i<n; i++){
+			y  = times(y, x);
+		}
+	}
+		else if (n < 0) {
+			int trick = n - n - n;
+			for (int i=2; i<trick; i++){
+				y  = times(y, x);
+			}
+			double z = (double)y;
+			return 1/z;	
+		}
+	return y;
+
+}
+	
+
+	// Returns the integer part of x1 / x2 6/-2 -2/6
+	public static double div(int x1, int x2) {
+		double z = 0;
+		if (x1 >= x2 ) {
+			while (x1 >= x2) {
 			x1 = minus(x1, x2);
-			z++;
-		}		
+			z++; 
+		}
+		}
+		else{
+		while (x1 <= x2) {
+			x2 = minus(x2, x1);
+			z--;
+		}	
+	}
+		if (x1 < 0 || x2 < 0)
+			return -z;	
 		return z;
 	}
 
